8104a1cb3e79b51f10a25cfd11557d02dc1b1e6d
Xhintu098 <sastisadatdn@gmail.com>
2024-08-31 20:34:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7245510353:AAGGUmUCJN6vm5Tfm_JnN94LygkOQyhhO4A")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("venomXcrazy")
+OWNER_USERNAME=("CrazyXrohanXyZ")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
-ADMIN_IDS=("5588464519")
+ADMIN_IDS=("6508557076")
 
 
 
